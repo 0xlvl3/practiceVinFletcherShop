8f619fa53957aa9110a56ec6695e3e31b965079d
0xlvl3 <cryptochoccy@gmail.com>
2022-05-05 19:20:28 +1000

    redo GetLength, TotalCost tomorrow make it work properly

--- Program.cs
+++ Program.cs
@@ -10,29 +10,47 @@ Console.WriteLine($"\nWelcome {user} have a look at my options for an arrow and 
 Arrow arrow = new Arrow(ArrowHeadType.Steel, FletchingType.Plastic, 60);
 
 ArrowHeadType arrowHead = GetArrowHead();
-Console.WriteLine($"{arrowHead}");
+Console.WriteLine($"\n{arrowHead}");
 
 FletchingType fletchingType = GetFletchingType();
-Console.WriteLine($"{fletchingType}");
+Console.WriteLine($"\n{fletchingType}");
 
 float length = GetLength();
+Console.WriteLine($"{user} you have picked {length} that will be {LengthCost()} gold");
 
+Console.WriteLine($"\n{user} you have chosen, {arrowHead} arrow head with a {fletchingType} fletch with a length of {length}");
+Console.Write($"That will come to {totalCost()}");
+
+
+float totalCost()
+{
+    float cost = 0;
+    if(arrowHead == ArrowHeadType.Steel) { cost += 10; }
+    else if (arrowHead == ArrowHeadType.Wood) { cost += 3; }
+    else { cost += 5; }
+
+    if (fletchingType == FletchingType.Plastic) { cost += 10; }
+    else if (fletchingType == FletchingType.Plastic) { cost += 3; } else { cost += 5; }
+
+    cost += length;
+
+    return cost;
+}
+
+Console.WriteLine("stop");
+Console.ReadKey();
+//method to get userlength return
 float GetLength()
 {
-
-    float userLength;
-    while(true)
+    float userLength = 0;
+    Console.WriteLine($"{user} please enter a length between 60 - 100");
+    while (userLength < 60 || userLength > 100)
     {
-        Console.WriteLine("Please pick the length of your arrow - between 60 - 100cm");
         userLength = Convert.ToSingle(Console.ReadLine());
-        if (userLength <= 60 && userLength >= 100)
-        {
-            Console.WriteLine($"{user} that value is too high please try between 60 - 100");
-        }
-        return userLength;
+        Console.WriteLine($"{user} that is too big ");
     }
+    return userLength;
 }
-Console.WriteLine($"{arrow.Length}");
 
 FletchingType GetFletchingType()
 {
@@ -76,12 +94,19 @@ ArrowHeadType GetArrowHead()
     }
 }
 
-//Small method intro
+//method Title that runs at the start
 void Title()
 {
     Console.WriteLine("^^^^ VINS ARROWS MAKER ^^^^");
     Console.WriteLine("||||                   ||||");
     Console.WriteLine("\"\"\"\"                   \"\"\"\"");
+}
+
+float LengthCost()
+{
+    float cost = 0;
+    cost = 0.05f * length;
+    return cost;
 }
 
 //Arrow class
@@ -92,32 +117,32 @@ class Arrow
     //private FletchingType _fletchingType;
     //private float _length;
 
+    public static ArrowHeadType _arrowHeadType;
+    public static FletchingType _fletchingType;
+    static float _length;
+
+
     //constructor we will use to access the private variables
     public Arrow(ArrowHeadType arrowHeadType, FletchingType fletchingType, float length)
     {
 
-        ArrowHeadType = arrowHeadType;
-        FletchingType = fletchingType;
-        Length = length;
+        _arrowHeadType = arrowHeadType;
+        _fletchingType = fletchingType;
+        _length = length;
 
     }
 
     //getter methods
-    public ArrowHeadType ArrowHeadType { get; }
-    public FletchingType FletchingType { get; }
-    public float Length { get; }
+    public ArrowHeadType ArrowHeadType { get; set; }
+    public FletchingType FletchingType { get; set; }
+    public float Length { get; set; }
 
     //parameterless constructor
     public Arrow() { }
 
-    //float GetCost()
-    //{
-    //    float cost = 0;
-    //    cost += 0.05 * Length();
-    //}
 }
 
 
 //Enums
-enum ArrowHeadType { Steel, Wood, Obsidian }
-enum FletchingType { Plastic, TurkeyFeather, GooseFeather }
+enum ArrowHeadType { Steel = 10, Wood = 3, Obsidian = 5 }
+enum FletchingType { Plastic = 10, TurkeyFeather = 5, GooseFeather = 3 }
